0821814e3a1dc588d323a42a7835955710c4298a
Steve Clements <steve@originaleye.co.uk>
2014-04-24 11:55:46 +0100

    fixed failing test

--- test/test-creation.js
+++ test/test-creation.js
@@ -29,6 +29,7 @@ describe('seed generator', function () {
         helpers.mockPrompt(this.app, {
             'someOption': true
         });
+        this.app.randomSessionKey = 'test';
         this.app.options['skip-install'] = true;
         this.app.run({}, function () {
             helpers.assertFiles(expected);
