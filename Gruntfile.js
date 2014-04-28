8ced652e490f097f72b1e2e324255b7e9661da68
Steve Clements <steve@originaleye.co.uk>
2014-04-28 09:48:48 +0100

    modified upstream push destination

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function(grunt) {
         		commitFiles: ['package.json'],
         		createTag: false,
         		push: true,
-        		pushTo: 'origin/master'
+        		pushTo: 'origin'
 
         	}
         }
